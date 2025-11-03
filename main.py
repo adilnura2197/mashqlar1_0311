45c49c220368bd739cb355a5d135cbe2ff18a110
adilnura2197 <adilnura2197@gmail.com>
2025-11-03 17:38:03 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,84 @@
+#1-misol
+sum = 0
+for i in range(1, 11):
+    sum += i
+
+print(sum)
+
+
+#2-misol
+sum = 0
+for i in range(1, 51):
+    if i % 2 != 0:
+        sum += i
+
+print(sum)
+
+
+#3-misol
+sum = 0
+for i in range(2, 101):
+    if i % 2 == 0:
+        sum += i
+
+print(sum)
+
+
+#4-misol
+sum = 0
+for i in range(10, 31):
+    sum += i
+
+print(sum)
+
+
+#5-misol
+n = int(input("Son kirit: "))
+sum = 0
+
+for i in range(1, n+1):
+    sum += i
+
+print(sum)
+
+
+#6-misol
+sum = 0
+for i in range(1, 21):
+    sum += i
+
+print(sum * 2)
+
+
+#7-misol
+sum = 0
+for i in range(5, 26):
+    sum += i
+
+print(sum)
+
+
+#8-misol
+sum = 0
+
+for i in range(1, 16):
+    sum += i
+
+print(sum * 3)
+
+
+#9-misol
+sum = 0
+for i in range(50, 101):
+    sum += i
+
+print(sum)
+
+
+#10-misol
+sum = 0
+for i in range(1, 31):
+    sum += i
+
+print(sum * 5)
+
